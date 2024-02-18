cd61588aac957c848f4f38241e0dcba51981b241
ibrahim hajjar <ibrahim.hajjar@7d.com.kw>
2024-02-18 12:37:26 +0300

    add: basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar'; 
+import { Home } from './components/Home';
+import { Register } from './auth/Register';
+import { Login } from './auth/Login'; 
+import { TodoList } from './features/todos/TodoList'; 
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/todos" element={<TodoList />} /> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 

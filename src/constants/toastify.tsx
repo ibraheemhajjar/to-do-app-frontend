a20584946c4472d2baaa18cb9d32c76e55846f6b
ibrahim hajjar <ibrahim.hajjar@7d.com.kw>
2024-02-19 21:15:59 +0300

    add: toastify options

---
+++ src/constants/toastify.tsx
@@ -0,0 +1,8 @@
+import { ToastOptions } from "react-toastify";
+
+export const toastifyOptions: ToastOptions = {
+  position: "top-right",
+  autoClose: 5000,
+  closeOnClick: true,
+  pauseOnHover: true,
+};

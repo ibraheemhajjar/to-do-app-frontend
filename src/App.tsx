04885f8651d0cf2cd029d83e45ff73c4b0eb24c6
ibrahim hajjar <ibrahim.hajjar@7d.com.kw>
2024-02-19 21:13:35 +0300

    add: components import in app module

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar'; 
-import { Home } from './components/Home';
-import { Register } from './auth/Register';
-import { Login } from './auth/Login'; 
-import { TodoList } from './features/todos/TodoList'; 
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import TodoList from "./components/TodoList";
+import { userActions } from "./store";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import PrivateRoute from "./components/PrivateRoute";
+import { WebsocketProvider } from "./contexts/WebsocketContext";
+import { socket } from "./contexts/WebsocketContext";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/todos" element={<TodoList />} /> 
-      </Routes>
-    </BrowserRouter>
-  );
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const token = localStorage.getItem("accessToken");
+		if (token) {
+			dispatch(userActions.login(token));
+			dispatch(userActions.setAccessToken(token));
+		}
+	});
+
+	return (
+		<WebsocketProvider value={socket}>
+			<BrowserRouter>
+				<NavBar />
+				<Routes>
+					<Route path="/" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					<Route
+						path="/todos"
+						element={
+							<PrivateRoute>
+								<TodoList />
+							</PrivateRoute>
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</WebsocketProvider>
+	);
 }
 
 export default App;
